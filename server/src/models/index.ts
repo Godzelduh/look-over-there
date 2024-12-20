307cad1c62333623fca2343dac93ef46b2bff449
Bryspen <bryanmspencer@gmail.com>
2024-12-19 19:32:02 -0600

    Revert "Pb updates/queries"

--- server/src/models/index.ts
+++
@@ -1,5 +0,0 @@
-import User from "./User.js";
-import ChallengeProgress from "./ChallengeProgress.js";
-import Challenge from "./Challenge.js";
-
-export default {User, ChallengeProgress, Challenge};
