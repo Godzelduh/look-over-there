620a2c653b8e7bb388baebd63acb10ca82c94a9c
Patrick Bowman <patrickdbowman91@gmail.com>
2024-12-29 17:51:07 -0500

    Misc updates

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -53,10 +53,10 @@ const Home = () => {
       <img src={ScavengerHunting} alt="Found Locations" style={styles.image} />
       <form onSubmit={handleFormSubmit}>
         <div className='search-bar'>
-          <label htmlFor='searchbar'>Create your own Scavenger Hunt!</label>
+          <label htmlFor='searchbar' className='search-label'>Your Scavenger Hunt starts here!</label>
           <input
             type='text'
-            placeholder='Enter your City'
+            placeholder='          Enter your city'
             name='city'
             onChange={handleInputChange}
             className="log-in"
@@ -69,7 +69,7 @@ const Home = () => {
           type='submit'
           className="log-in"
         >
-          Search
+          Begin Scavenger Hunt
         </button>
       </form>
 
