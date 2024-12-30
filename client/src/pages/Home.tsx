f80deec0168446c9c96f9bcc52d09690ab0b8284
melanieprimpas <melanie.primpas@gmail.com>
2024-12-30 17:30:26 -0500

    seperated the css classes for the two carousels

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,16 @@
 import { CSSProperties } from 'react';
-import ScavengerHunting from '../assets/ScavengerHunting.avif'; // Import the image
+//import ScavengerHunting from '../assets/ScavengerHunting.avif'; // Import the image
 import type { ChangeEvent, FormEvent } from 'react';
 import { GET_PLACES } from '../utils/queries';
 import { useLazyQuery } from '@apollo/client';
 import { useState } from 'react';
 import CarouselImageReel from '../components/CarouselImageReel';
 import ChallengeCard from '../components/ChallangeCard';
+
+// import { useMutation } from '@apollo/client';
+// import { CREATE_CHALLENGE } from '../utils/mutations';
+// import { GET_ME } from '../utils/queries';
+import Auth from '../utils/auth';
 
 //import { text } from 'express';
 
@@ -46,6 +51,32 @@ const Home = () => {
     });
   }
 
+  const createChallange = async () => {
+    if (Auth.loggedIn()) {
+      /*const [createChallenge] = useMutation(CREATE_CHALLENGE, {
+        refetchQueries: [
+          GET_ME,
+          'me'
+        ]
+      });*/
+      const type = places.__typename;
+      const location  = {
+        type: places.geometry.__typename,
+        coordinates: [places.geometry.location.lat, places.geometry.location.lng]
+      }
+      const task = places.name;
+      const image_url = places.photos;
+      console.log(`Type: ${type}, Location: ${location}, Task: ${task}, Images: ${image_url}`)
+/*
+      try {
+        const { data } = await createChallenge({
+          variables: { input: { type, location, task, image_url }}
+        })
+      }*/
+
+    }
+  }
+
   if (called && loading) {
     return <div>Loading...</div>;
   }
@@ -76,7 +107,15 @@ const Home = () => {
       </form>
 
       {called && !loading && places.length > 0 && (
+        <div>
         <ChallengeCard places = {places}/>
+        <button
+          onClick={createChallange}
+          type='submit'
+          className="log-in"
+          >Start Challange!
+        </button>
+        </div>
       )}
     </div>
   );
