70222cb90b71542bdaf7f45b09972e19c4ceb9a5
melanieprimpas <melanie.primpas@gmail.com>
2025-01-05 21:26:36 -0500

    trying to push to develop

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -11,16 +11,63 @@ import { haversineDistance } from '../utils/locver';
 import { UPDATE_HUNT_PROGRESS } from '../utils/mutations';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth'; // Import the Auth module
+import { FaSyncAlt } from 'react-icons/fa';
 
 const Profile: React.FC = () => {
-  
-  const styles: { h3: CSSProperties } = {
+  const [flippedCard, setFlippedCard] = useState<{ [key: string]: boolean }>({});
+  const styles: { cardContainer: CSSProperties, h3: CSSProperties, cardFlip: CSSProperties, cardFace: CSSProperties, cardFront: CSSProperties, cardBack: CSSProperties, flipButton: CSSProperties } = {
     h3: {
       textAlign: 'left',
+    },
+    cardContainer: {
+      width: '300px',
+      height: '350px',
+      margin: '15px',
+ 
+    },
+    cardFlip: {
+      position: 'relative',
+      width: '100%',
+      height: '100%',
+      transformStyle: 'preserve-3d',
+      transition: 'transform 0.8s',
+    },
+    cardFace: {
+      
+      width: '100%',
+      height: '100%',
+      backfaceVisibility: 'hidden',
+      borderRadius: '10px',
+      boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
+    },
+    cardFront: {
+      backgroundColor: 'grey',
+    },
+    cardBack: {
+      
+      backgroundColor: '#f8f9fa',
+      transform: 'rotateY(180deg)',
+      padding: '20px',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-around',
+    },
+    flipButton: {
+      position: 'absolute',
+      top: '10px',
+      right: '10px',
+      fontSize: '24px',
+      cursor: 'pointer',
+      color: 'white',
+      backgroundColor: 'rgba(0, 0, 0, 0.6)',
+      padding: '5px',
+      borderRadius: '50%',
+      zIndex: 1,
     }
+
   };
 
- 
+
   //  Query to fetch challenges
   // const { loading, error, data } = useQuery(GET_CHALLENGES);
 
@@ -30,6 +77,7 @@ const Profile: React.FC = () => {
   // const [markChallengeComplete] = useMutation(MARK_CHALLENGE_COMPLETE);
   const [fetchHuntsByUser, { loading, error, data }] = useLazyQuery(GET_HUNTS_BY_USER);
   const [updateHuntProgress] = useMutation(UPDATE_HUNT_PROGRESS);
+
   // Fetch user's GPS location
   useEffect(() => {
     const fetchData = async () => {
@@ -85,13 +133,16 @@ const Profile: React.FC = () => {
 
   }, [fetchHuntsByUser]);
 
+  const handleFlip = (challengeId: string) => {
+    setFlippedCard(prev => ({
+      ...prev,
+      [challengeId]: !prev[challengeId]
+    }));
+  };
   console.log("User Location: ", userLocation); // Debug the user's location
 
-  // Handle loading state
-  if (loading) return <p>Loading challenges...</p>;
 
-  // Handle error state
-  if (error) return <p>Error fetching challenges: {error.message}</p>;
+
 
   // Destructure the challenges from data
   // const challenges = data?.getChallenges || [];
@@ -130,52 +181,97 @@ const Profile: React.FC = () => {
     }
   };
 
+  // Handle loading state
+  if (loading) return <p>Loading challenges...</p>;
+
+  // Handle error state
+  if (error) return <p>Error fetching challenges: {error.message}</p>;
+
   return (
     <div className="profile-container">
       <div className="clouds">
         <img src={clouds} alt="clouds" className="cloud-image" />
       </div>
-      
+
       <h1>Your Active Hunts</h1>
 
       {hunts.map((hunt: any) => (
         <React.Fragment key={hunt.id}>
           <h3 style={styles.h3}>Hunt for {hunt.city}:</h3>
-          
+
           <div className="scroll-container">
             <div className="card-container">
               {hunt.challenges.map((challenge: any) => {
                 const distance = userLocation
                   ? haversineDistance(
-                      userLocation.lat,
-                      userLocation.lng,
-                      challenge.location.coordinates[1],
-                      challenge.location.coordinates[0]
-                    )
+                    userLocation.lat,
+                    userLocation.lng,
+                    challenge.location.coordinates[1],
+                    challenge.location.coordinates[0]
+                  )
                   : null;
 
                 const isNear = distance !== null && distance <= 5000;
                 const isCompleted = challenge.status === 'completed';
-                return (
-                  <div key={challenge.challenge_id} className="challenge-card">
-                    <img
-                      src={challenge.image_url || 'default-placeholder.jpg'}
-                      alt={challenge.name}
-                      className={`challenge-image ${isCompleted ? 'completed-image' : ''}`}
+                const isFlipped = flippedCard[challenge.challenge_id];
 
-                    />
-                      {isCompleted && (
-                        <div className="checkmark-overlay">
-                           <span>&#10003;</span>
-                        </div>
-                      )}
-                    <button
-                      onClick={() => handleCompleteChallenge(hunt.id, challenge.challenge_id)}
-                      disabled={!isNear || isCompleted}
-                      className={`verify-button ${isCompleted ? 'completed' : isNear ? 'near' : 'not-near'}`}
+                return (
+                  <div style={styles.cardContainer} key={challenge.challenge_id}>
+                    <div
+                      style={{
+                        ...styles.cardFlip,
+                        transform: isFlipped ? 'rotateY(180deg)' : 'rotateY(0deg)'
+                      }}
                     >
-                      {isCompleted ? 'Completed' : isNear ? 'In Range, Verify Location' : 'Not Near'}
-                    </button>
+                      {/* Front side of the Card */}
+                      <div style={{...styles.cardFace, ...styles.cardFront}} className="challenge-card">
+                        <img
+                          src={challenge.image_url || 'default-placeholder.jpg'}
+                          alt={challenge.name}
+                          className={`challenge-image ${isCompleted ? 'completed-image' : ''}`}
+                        />
+                        {isCompleted && (
+                          <div className="checkmark-overlay">
+                            <span>&#10003;</span>
+                          </div>
+                        )}
+                        <button
+                          onClick={() => handleCompleteChallenge(hunt.id, challenge.challenge_id)}
+                          disabled={!isNear || isCompleted}
+                          className={`verify-button ${isCompleted ? 'completed' : isNear ? 'near' : 'not-near'}`}
+                        >
+                          {isCompleted ? 'Completed' : isNear ? 'In Range, Verify Location' : 'Not Near'}
+                        </button>
+                        <div
+                          onClick={() => handleFlip(challenge.challenge_id)}
+                          style={styles.flipButton}
+                        >
+                          <FaSyncAlt />
+                        </div>
+                      </div>
+
+                      {/* Back side of the Card */}
+                      <div style={{...styles.cardFace, ...styles.cardBack}}>
+                        <h3>Looking for a hint?</h3>
+                        <div>
+                          <h4>Name of Attraction: </h4>
+                          <span>{challenge.name}</span>
+                          <button className="log-in">Click to reveal!</button>
+                        </div>
+                        <div>
+                          <h4>Distance from you to Location: </h4>
+                          <span>{distance} meters </span>
+                          <button className="log-in">Click to reveal!</button>
+                        </div>
+
+                        <div
+                          onClick={() => handleFlip(challenge.challenge_id)}
+                          style={styles.flipButton}
+                        >
+                          <FaSyncAlt />
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 );
               })}
